eb5dfba85162c637ef7f0771b842ae1b42eb99fa
Robespierre Triviño <robtrivi@espol.edu.ec>
2025-06-17 09:51:10 -0500

    feat: agregar función para cargar estrategias en ActivitiesConfig

--- activities/apps.py
+++ activities/apps.py
@@ -1,6 +1,14 @@
+from pathlib import Path
+
 from django.apps import AppConfig
+
+from utils.module_loader import import_strategies
 
 
 class ActivitiesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'activities'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "activities"
+
+    def ready(self):
+        base_path = Path(__file__).resolve().parent / "strategies"
+        import_strategies("payload", base_path.parent)
