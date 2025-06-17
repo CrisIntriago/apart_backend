d52729046e18a9b8a26237519010ac858afd3af8
Robespierre Triviño <robtrivi@espol.edu.ec>
2025-06-17 13:41:57 -0500

    feat: agregar ruta para la vista de envío de respuestas de actividades

--- activities/urls.py
+++ activities/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from .views import ActivityListView
+from .views import ActivityListView, SubmitAnswerView
 
 urlpatterns = [
     path("", ActivityListView.as_view(), name="activity-list"),
+    path(
+        "<int:activity_id>/submit/",
+        SubmitAnswerView.as_view(),
+        name="submit-answer",
+    ),
 ]
