7f606097793cb214992983c7faf89ef455ba4d5d
Robespierre Triviño <robtrivi@espol.edu.ec>
2025-06-17 12:22:24 -0500

    feat: agregar vista para la presentación de respuestas de actividades

--- activities/views.py
+++ activities/views.py
@@ -6,6 +6,7 @@ from rest_framework.views import APIView
 
 from activities.models.base import Activity
 from activities.serializers import ActivityListSerializer
+from activities.services import AnswerSubmissionService
 
 
 class ActivityListView(APIView):
@@ -18,3 +19,13 @@ class ActivityListView(APIView):
         activities = Activity.objects.all().order_by("created_at")
         serializer = ActivityListSerializer(activities, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class SubmitAnswerView(APIView):
+    def post(self, request, activity_id):
+        try:
+            service = AnswerSubmissionService(request.user, activity_id, request.data)
+            is_correct = service.execute()
+            return Response({"is_correct": is_correct}, status=status.HTTP_200_OK)
+        except ValueError as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
